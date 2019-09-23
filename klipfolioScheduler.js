2f07834af0637ea5310c8af1d43a513ab0c344e6
alex <43148847+MugssyBoy@users.noreply.github.com>
2019-09-24 04:52:37 +0800

    Update klipfolioScheduler.js

--- klipfolioScheduler.js
+++ klipfolioScheduler.js
@@ -1,6 +1,6 @@
 const schedule = require('node-schedule');
 const klipfolioDataRefresher = require('./klipfolioDataRefresher.js');
 
-const klipfolioRefreshSchedule = schedule.scheduleJob('0 13 * * *', function() {
+const klipfolioRefreshSchedule = schedule.scheduleJob('0 13 * * 0-6', function() {
 	klipfolioDataRefresher.processKlipfolioLogin();
 });
